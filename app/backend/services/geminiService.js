f2493720e6d3ec95733997939ccc32c0dd810159
kennethsteuart <krs00046@mix.wvu.edu>
2025-03-01 21:41:19 -0500

    hi

--- app/backend/services/geminiService.js
+++ app/backend/services/geminiService.js
@@ -7,7 +7,7 @@ const analyzeEmail = async (emailContent) => {
     try {
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-        const text = `Analyze this email and classify it as 'Phishing' or 'Not Phishing'. Email content: "${emailContent}"`;
+        const text = `Analyze this email and classify it as None to Minimal Phishing, Liking Present Phishing, and Phishing Obviously Present. Give it a score of 1 through 10. A score of 1-3 will fit None to Minimal Phishing. A score of 4-7 will fit Likely Present Phishing. A score of 8-10 will fit Phishing Obviously Present. When giving back the response based on your analysis of the email, only respong with the singular number. Email content: "${emailContent}"`;
 
         const result = await model.generateContent(text);
 
